7bf7dd8e59443ae5b362ed1fe40e135eb2a6efdb
Chris Praktish & Susan Yi <pair+cprakti.yisusans@devbootcamp.com>
2016-06-10 11:54:09 -0400

    print show title name when you get an input from user

--- public/js/application.js
+++ public/js/application.js
@@ -2,35 +2,44 @@
 // https://api-public.guidebox.com/v1.43/US/jpoBwnzeZ3kKrwKVt1oBGcJE5Z9y0f/search/title/Arrested%252520Development/fuzzy
 var Tv = function(args) {
   this.title = args.title || "unknown";
-  this.first_aired = args.first_aired || "unknown";
+  this.id = args.id || "unknown";
+}
+
+var availableContent = function(args) {
+  this.available_content = args.available_content || "unknown";
 }
 
 
-var shows = [];
 $(document).ready (function(){
-  var API_KEY = "jpoBwnzeZ3kKrwKVt1oBGcJE5Z9y0f";
-  var BASE_URL = "https://api-public.guidebox.com/v1.43/US/";
-  var TITLE = "Game of Thrones";
-  var search_url = BASE_URL + API_KEY + "/search/title/" + TITLE + "/fuzzy";
+
+var shows = [];
+var sources = [];
+
+  $('#title-input').on ('submit', function(event) {
+    event.preventDefault();
+
+    $target = $(event.target)
+    var API_KEY = "jpoBwnzeZ3kKrwKVt1oBGcJE5Z9y0f";
+    var BASE_URL = "https://api-public.guidebox.com/v1.43/US/";
+    var TITLE = $target.find("input").val();
+    var search_url_title_id = BASE_URL + API_KEY + "/search/title/" + TITLE + "/fuzzy";
 
   $.ajax({
-    url: search_url,
+    method: "GET",
+    url: search_url_title_id,
     dataType: 'json'
-  }).done(function(response){
+    }).done(function(response){
     return response.results.forEach(function(item){
       return shows.push( new Tv(item) )
     })
-
-  }).fail(function(error) {
-
-  }).then(function(resp){
+    }).then(function(resp){
       $("#title").append(shows[0].title)
       $("#first_aired").append(shows[0].first_aired)
+  });
+      debugger
 
   });
 
 
 
-
-
-})
+});
